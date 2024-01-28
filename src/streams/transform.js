58c392fb0daf40c46e9975d7ba596d2566ac5713
AntonFartovii <anton@rza.by>
2024-01-28 20:18:27 +0300

    feat: streams

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,34 @@
-const transform = async () => {
-    // Write your code here 
+import {Transform, pipeline} from 'stream';
+import {EOL} from 'os';
+import {stdin, stdout} from 'node:process';
+
+class TransformStream extends Transform {
+
+    constructor(options = {}) {
+        super (options);
+    }
+
+    _transform(chunk, enc, cb) {
+        const chunkStringified = chunk.toString();
+        const reversedChunk = chunkStringified.replace(EOL, '').split('').reverse().join('')+EOL;
+        this.push(reversedChunk);
+        cb();
+    }
+}
+
+const transform = async ()=> {
+    const transform = new TransformStream();
+    pipeline (
+        stdin,
+        transform,
+        stdout,
+        err => {
+            console.log(err)
+        }
+    );
+    stdout.write('Write anythings...\n');
 };
 
 await transform();
+
+// npm run streams:transform
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,19 @@
+import {createWriteStream} from 'fs';
+import {stdin, stdout} from 'node:process';
+import {getFilePath} from '../utils.js';
+
+const url = import.meta.url;
+
 const write = async () => {
-    // Write your code here 
+    const filepath = getFilePath(url, 'files/fileToWrite.txt');
+    const writeStream = createWriteStream(filepath);
+    stdin.pipe(writeStream);
+    stdout.write('Write anything to file fileToWrite.txt .....\n');
 };
 
 await write();
+
+// npm run streams:write
+
+// write.js - implement function that writes process.stdin data
+// into file fileToWrite.txt content using Writable Stream
