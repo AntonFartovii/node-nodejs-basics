70dbbe01fb30a24d928715b5fe9db81a73028668
AntonFartovii <anton@rza.by>
2024-01-29 13:11:44 +0300

    feat: zlib

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,19 @@
+import {stdout} from 'node:process';
+import {createReadStream,createWriteStream} from 'fs';
+import {createGzip} from 'zlib';
+import {getFilePath} from '../utils.js';
+const url = import.meta.url;
+
 const compress = async () => {
-    // Write your code here 
+    const readStream = createReadStream(getFilePath(url, 'files/fileToCompress.txt'));
+    const writeStream = createWriteStream(getFilePath(url, 'files/archive.gz'));
+    const gzip = createGzip();
+    readStream.pipe(gzip).pipe(writeStream);
+    stdout.write('Compress done!\n');
 };
 
 await compress();
+
+// npm run zip:compress
+
+// compress.js - implement function that compresses file fileToCompress.txt to archive.gz using zlib and Streams API
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,21 @@
+import {stdout} from 'node:process';
+import {createReadStream, createWriteStream} from 'fs';
+import {createUnzip} from 'zlib';
+import {getFilePath} from '../utils.js';
+const url = import.meta.url;
+
 const decompress = async () => {
-    // Write your code here 
+    const readStream = createReadStream(getFilePath(url, 'files/archive.gz'));
+    const writeStream = createWriteStream(getFilePath(url, 'files/fileToCompress.txt'));
+    const gzip = createUnzip();
+    readStream.pipe(gzip).pipe(writeStream);
+    stdout.write('Decompress done!\n');
 };
 
 await decompress();
+
+// npm run zip:decompress
+
+// decompress.js - implement function that decompresses archive.gz
+// back to the fileToCompress.txt with same content as before
+// compression using zlib and Streams API
